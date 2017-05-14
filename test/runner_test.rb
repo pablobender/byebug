c257502eeb87d64525e0279439097e87a311ee75
David Rodríguez <deivid.rodriguez@riseup.net>
2017-05-14 17:32:43 -0300

    Migrate runner tests from mocking to reality

--- test/runner_test.rb
+++ test/runner_test.rb
@@ -1,74 +1,73 @@
 require 'test_helper'
-require 'byebug/runner'
 
 module Byebug
   #
   # Tests standalone byebug when flags that require no target program are used
   #
   class RunnerWithoutTargetProgramTest < TestCase
-    def setup
-      super
-
-      runner.interface = Context.interface
-    end
-
     def test_run_with_version_flag
-      with_command_line('bin/byebug', '--version') { runner.run }
+      stdout = run_program('bin/byebug --version')
 
-      check_output_includes(/#{Byebug::VERSION}/)
+      assert_match(/Running byebug 9.0.6/, stdout)
     end
 
     def test_run_with_help_flag
-      with_command_line('bin/byebug', '--help') { runner.run }
+      stdout = run_program("#{binstub} --help")
 
-      check_output_includes(
-        /-d/, /-I/, /-q/, /-s/, /-x/, /-m/, /-r/, /-R/, /-t/, /-v/, /-h/
-      )
+      expected_stdout = <<-TEXT.gsub(/^ {6}/, '')
+
+        byebug 9.0.6
+
+        Usage: byebug [options] <script.rb> -- <script.rb parameters>
+
+          -d, --debug               Set $DEBUG=true
+          -I, --include list        Add to paths to $LOAD_PATH
+          -m, --[no-]post-mortem    Use post-mortem mode
+          -q, --[no-]quit           Quit when script finishes
+          -x, --[no-]rc             Run byebug initialization file
+          -s, --[no-]stop           Stop when script is loaded
+          -r, --require file        Require library before script
+          -R, --remote [host:]port  Remote debug [host:]port
+          -t, --[no-]trace          Turn on line tracing
+          -v, --version             Print program version
+          -h, --help                Display this message
+
+      TEXT
+
+      assert_equal expected_stdout, stdout
     end
 
     def test_run_with_remote_option_only_with_a_port_number
-      with_command_line('bin/byebug', '--remote', '9999') do
-        assert_calls(Byebug, :start_client, 'localhost 9999') { runner.run }
-      end
+      stdout = run_program("#{binstub} --remote 9999")
+
+      assert_match(/Connecting to byebug server at localhost:9999.../, stdout)
     end
 
     def test_run_with_remote_option_with_host_and_port_specification
-      with_command_line('bin/byebug', '--remote', 'myhost:9999') do
-        assert_calls(Byebug, :start_client, 'myhost 9999') { runner.run }
-      end
+      stdout = run_program("#{binstub} --remote myhost:9999")
+
+      assert_match(/Connecting to byebug server at myhost:9999.../, stdout)
     end
 
     def test_run_without_a_script_to_debug
-      with_command_line('bin/byebug') do
-        runner.run
+      stdout = run_program(binstub)
 
-        check_error_includes 'You must specify a program to debug'
-      end
+      assert_match(/\*\*\* You must specify a program to debug/, stdout)
     end
 
     def test_run_with_an_nonexistent_script
-      with_command_line('bin/byebug', 'non_existent_script.rb') do
-        runner.run
+      stdout = run_program("#{binstub} non_existent_script.rb")
 
-        check_error_includes "The script doesn't exist"
-      end
+      assert_match(/\*\*\* The script doesn't exist/, stdout)
     end
 
     def test_run_with_an_invalid_script
       example_file.write('[1,2,')
       example_file.close
 
-      with_command_line('bin/byebug', example_path) do
-        runner.run
+      stdout = run_program("#{binstub} #{example_path}")
 
-        check_error_includes 'The script has incorrect syntax'
-      end
-    end
-
-    private
-
-    def runner
-      @runner ||= Byebug::Runner.new(false)
+      assert_match(/\*\*\* The script has incorrect syntax/, stdout)
     end
   end
 
@@ -84,129 +83,103 @@ module Byebug
     end
 
     def test_run_with_a_script_to_debug
-      with_command_line('bin/byebug', example_path) do
-        non_stop_runner.run
+      stdout = run_program(
+        "#{binstub} #{example_path}",
+        'puts "Program: #{$PROGRAM_NAME}"'
+      )
 
-        assert_equal $PROGRAM_NAME, example_path
-      end
+      assert_match(/Program: #{example_path}/, stdout)
     end
 
     def test_run_with_a_script_and_params_does_not_consume_script_params
-      with_command_line('bin/byebug', '--', example_path, '-opt', 'value') do
-        non_stop_runner.run
+      stdout = run_program(
+        "#{binstub} -- #{example_path} -opt value",
+        'puts "Args: #{ARGV.join(\', \')}"'
+      )
 
-        assert_equal ['-opt', 'value'], $ARGV
-      end
+      assert_match(/Args: -opt, value/, stdout)
     end
 
     def test_run_with_ruby_script_ruby_is_ignored_and_script_passed_instead
-      with_command_line('bin/byebug', '--', 'ruby', example_path) do
-        non_stop_runner.run
+      stdout = run_program(
+        "#{binstub} -- ruby #{example_path}",
+        'puts "Program: #{$0}"'
+      )
 
-        assert_equal example_path, $PROGRAM_NAME
-      end
+      assert_match(/Program: #{example_path}/, stdout)
     end
 
     def test_run_with_post_mortem_mode_flag
-      with_setting :post_mortem, false do
-        with_command_line('bin/byebug', '-m', example_path) do
-          non_stop_runner.run
+      stdout = run_program("#{binstub} -m #{example_path}", 'show post_mortem')
 
-          assert_equal true, Setting[:post_mortem]
-        end
-      end
+      assert_match(/post_mortem is on/, stdout)
     end
 
     def test_run_with_linetracing_flag
-      with_setting :linetrace, false do
-        with_command_line('bin/byebug', '-t', example_path) do
-          non_stop_runner.run
+      stdout = run_program("#{binstub} -t #{example_path}", 'show linetrace')
 
-          assert_equal true, Setting[:linetrace]
-        end
-      end
+      assert_match(/linetrace is on/, stdout)
     end
 
     def test_run_with_no_quit_flag
       skip
+      stdout = run_program("#{binstub} --no-quit #{example_path}", 'quit!')
 
-      with_command_line('bin/byebug', '--no-quit', example_path) do
-        non_stop_runner.run
-
-        check_output_includes('(byebug:ctrl)')
-      end
+      assert_match(/\(byebug:ctrl\)/, stdout)
     end
 
     def test_run_with_require_flag
-      with_command_line('bin/byebug', '-r', 'abbrev', example_path) do
-        non_stop_runner.run
-      end
+      stdout = run_program(
+        "#{binstub} -r abbrev #{example_path}",
+        'puts "Abbrev loaded? #{$LOADED_FEATURES.last.include?(\'abbrev\')}"'
+      )
 
-      hsh = { 'can' => 'can', 'cat' => 'cat' }
-      assert_equal hsh, %w[can cat].abbrev
+      assert_match(/Abbrev loaded\? true/, stdout)
     end
 
     def test_run_with_a_single_include_flag
-      with_command_line('bin/byebug', '-I', 'dir1', example_path) do
-        non_stop_runner.run
-      end
+      stdout = run_program(
+        "#{binstub} -I dir1 #{example_path}",
+        'puts "dir1 in LOAD_PATH? #{$LOAD_PATH.include?(\'dir1\')}"'
+      )
 
-      assert_includes $LOAD_PATH, 'dir1'
+      assert_match(/dir1 in LOAD_PATH\? true/, stdout)
     end
 
     def test_run_with_several_include_flags
-      with_command_line('bin/byebug', '-I', 'dir1:dir2', example_path) do
-        non_stop_runner.run
-      end
+      stdout = run_program(
+        "#{binstub} -I d1:d2 #{example_path}",
+        'puts "d1 and d2 in LOAD_PATH? #{(%w(d1 d2) - $LOAD_PATH).empty?}"'
+      )
 
-      assert_includes $LOAD_PATH, 'dir1'
-      assert_includes $LOAD_PATH, 'dir2'
+      assert_match(/d1 and d2 in LOAD_PATH\? true/, stdout)
     end
 
     def test_run_with_debug_flag
-      with_command_line('bin/byebug', '-d', example_path) do
-        non_stop_runner.run
-      end
+      stdout = run_program(
+        "#{binstub} -d #{example_path}",
+        'puts "Debug flag is #{$DEBUG}"'
+      )
 
-      assert_equal $DEBUG, true
-      $DEBUG = false
+      assert_match(/Debug flag is true/, stdout)
     end
 
     def test_run_stops_at_the_first_line_by_default
-      enter 'cont'
-      with_command_line('bin/byebug', example_path) { stop_first_runner.run }
+      stdout = run_program("#{binstub} #{example_path}")
 
-      check_output_includes '=> 1: sleep 0'
+      assert_match(/=> 1: sleep 0/, stdout)
     end
 
     def test_run_with_no_stop_flag_does_not_stop_at_the_first_line
-      non_stop_runner.interface = Context.interface
+      stdout = run_program("#{binstub} --no-stop #{example_path}")
 
-      with_command_line('bin/byebug --no-stop', example_path) do
-        non_stop_runner.run
-      end
-
-      assert_empty non_stop_runner.interface.output
+      refute_match(/=> 1: sleep 0/, stdout)
     end
 
     def test_run_with_stop_flag_stops_at_the_first_line
-      enter 'cont'
+      stdout = run_program("#{binstub} --stop #{example_path}")
 
-      with_command_line('bin/byebug --stop', example_path) do
-        stop_first_runner.run
-      end
-
-      check_output_includes '=> 1: sleep 0'
-    end
-
-    private
-
-    def non_stop_runner
-      @non_stop_runner ||= Byebug::Runner.new(false)
-    end
-
-    def stop_first_runner
-      @stop_first_runner ||= Byebug::Runner.new(true)
+      assert_match(/=> 1: sleep 0/, stdout)
     end
   end
 end
